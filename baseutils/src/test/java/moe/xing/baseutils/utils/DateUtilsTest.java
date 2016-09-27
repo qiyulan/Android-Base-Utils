65b91f6b0a76cb874715da857e8a418730dbbfcf
祈星辰 <qiyulan@niconiconi.moe>
2016-09-27 17:32:25 +0800

    test ( utils ): setup test

---
+++ baseutils/src/test/java/moe/xing/baseutils/utils/DateUtilsTest.java
@@ -0,0 +1,153 @@
+package moe.xing.baseutils.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by hehanbo on 16-9-27.
+ * <p>
+ * Test for {@link DateUtils}
+ */
+public class DateUtilsTest {
+    @Test
+    public void getPastDateString() throws Exception {
+
+    }
+
+    @Test
+    public void getUnixStart() throws Exception {
+        assertEquals("1970-1-1", DateUtils.getUnixStart());
+    }
+
+    @Test
+    public void addMonth() throws Exception {
+
+    }
+
+    @Test
+    public void getNow() throws Exception {
+
+    }
+
+    @Test
+    public void getTodayAndClearTime() throws Exception {
+
+    }
+
+    @Test
+    public void getDate() throws Exception {
+
+    }
+
+    @Test
+    public void clearTime() throws Exception {
+
+    }
+
+    @Test
+    public void clearTime1() throws Exception {
+
+    }
+
+    @Test
+    public void getCalendarAndClearTime() throws Exception {
+
+    }
+
+    @Test
+    public void getCalendar() throws Exception {
+
+    }
+
+    @Test
+    public void betweenDateByDay() throws Exception {
+
+    }
+
+    @Test
+    public void format() throws Exception {
+
+    }
+
+    @Test
+    public void formatTo4yMMddHHmmss() throws Exception {
+
+    }
+
+    @Test
+    public void formatTo4y_MM_dd() throws Exception {
+
+    }
+
+    @Test
+    public void formatTo4y_MM_dd_HH_mm_ss() throws Exception {
+
+    }
+
+    @Test
+    public void formatToHH_mm_ss() throws Exception {
+
+    }
+
+    @Test
+    public void formatToHH_mm() throws Exception {
+
+    }
+
+    @Test
+    public void parse() throws Exception {
+
+    }
+
+    @Test
+    public void getCalendar1() throws Exception {
+
+    }
+
+    @Test
+    public void getCalendar2() throws Exception {
+
+    }
+
+    @Test
+    public void getFLDatesInMonth() throws Exception {
+
+    }
+
+    @Test
+    public void isDateInMonth() throws Exception {
+
+    }
+
+    @Test
+    public void isDateInMonth1() throws Exception {
+
+    }
+
+    @Test
+    public void getWeek() throws Exception {
+
+    }
+
+    @Test
+    public void getDaysOfMonth() throws Exception {
+
+    }
+
+    @Test
+    public void getDaysOfMonth1() throws Exception {
+
+    }
+
+    @Test
+    public void getFirstDayOfMonth() throws Exception {
+
+    }
+
+    @Test
+    public void getLastDayOfMonth() throws Exception {
+
+    }
+
+}
