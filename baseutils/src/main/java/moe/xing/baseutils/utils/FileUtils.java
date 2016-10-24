fdf590ba1a1ebe6f082145d10d705bcc9cefbd0c
祈星辰 <qiyulan@niconiconi.moe>
2016-10-24 15:46:17 +0800

    fix build error

--- baseutils/src/main/java/moe/xing/baseutils/utils/FileUtils.java
+++ baseutils/src/main/java/moe/xing/baseutils/utils/FileUtils.java
@@ -8,6 +8,8 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
+import com.tbruyelle.rxpermissions.RxPermissions;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
